296a28a276219842a212b67418ff2371dff40429
comptpbio <anyela.camargorodriguez@niab.com>
2021-07-21 13:00:56 +0100

    modified climate_model.R

--- climate_model.R
+++ climate_model.R
@@ -4,66 +4,68 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 #source('../aquacropr/R/Aqua_library.R')
+#change ARV
 
+my_path <- "C:/Users/Angelita/Documents/R/Cacao"
+setwd(my_path)
 
 
 transform_weather <- function(fname_in, fname_out){
   
   
+  fname_in <- 'POWER_SinglePoint_DailySantander.csv'
+  weather_raw <- read.csv(fname_in, skip = 17, header = TRUE)
   
-  #fname_in <- 'weather SIMPLE/Original POWER_SinglePoint_Daily_20180101_.csv'
-  weather_raw <- read.csv(fname_in, skip = 20, header = TRUE)
+  #dat <- data.frame(sprintf('%02d', weather_raw$YEAR  %% 100))                
   
- 
-  DATE <- paste(paste(strsplit(as.character(weather_raw$YEAR), split ='')[[1]][3:4], 
-                      collapse = ''), sprintf("%03d", weather_raw$DOY), sep = '')
   
-  weather_filter <- data.frame(cbind(DATE, sRAD = weather_raw[['ALLSKY_SFC_SW_DWN']],
+  DATE <- paste(paste(sprintf('%02d', weather_raw$YEAR  %% 100), 
+                      sprintf("%03d", weather_raw$DOY), sep = ''))
+  
+  weather_filter <- data.frame(cbind(DATE, SRAD = weather_raw[['ALLSKY_SFC_SW_DWN']],
                                      TMAX = weather_raw[['T2M_MAX']],
                                      TMIN = weather_raw[['T2M_MIN']],
                                      RAIN = weather_raw[['PRECTOT']]))
   
-  write.table(weather_filter, file = fname_out, sep=' ',
+  
+  
+  write.table(weather_filter, file = fname_out, sep='  ',
               quote = FALSE, row.names = FALSE)
   
-  
-  
-  
 }
 
-
-plot_data <- function(sdata){
-  
-  
-  
-  
-  #yy$Date <- droplevels(yy$Date)
-  # pdf('climate_plots.pdf', width=20)
-  # plot(y$Date, y$flowering, col='black', lwd=6, cex.axis=0.8, xaxt = "n", 
-  #      ylim = c(0, 110))
-  # lines(y$Date, y$rain,  col='red')
-  # lines(y$Date, y$RH,  col='blue')
-  # lines(y$Date, y$SRAD,  col='purple')
-  # lines(y$Date, y$WS2M*150,  col='brown')
-  # axis(1, at = seq(1,nrow(y), 15), labels= s$Date[seq(1, nrow(y), by=15)],
-  #      las=2, cex.axis=0.8)
-  # legend(1, 75, legend=c("Peso.cosechado.seco..kg.","rain","RH",
-  #                        "SRAD","WS2M"),
-  #        col=c("black","green", "red", "blue", "purple","brown"), lty=1:2, cex=0.8,
-  #        lwd=3)
-  # dev.off()
-  
-}
-
-
-fname_in <- 'weather SIMPLE/Original POWER_SinglePoint_Daily_20180101_.csv'
-fname_out <- 'weather SIMPLE/dummy_weather.WTH'
-
+fname_in <- 'weather_filter'
+fname_out <- 'dummy_weather.WTH'
 transform_weather(fname_in, fname_out)
+#-------------------------
 
 
 
 
+
+#yy$Date <- droplevels(yy$Date)
+# pdf('climate_plots.pdf', width=20)
+# plot(y$Date, y$flowering, col='black', lwd=6, cex.axis=0.8, xaxt = "n", 
+#      ylim = c(0, 110))
+# lines(y$Date, y$rain,  col='red')
+# lines(y$Date, y$RH,  col='blue')
+# lines(y$Date, y$SRAD,  col='purple')
+# lines(y$Date, y$WS2M*150,  col='brown')
+# axis(1, at = seq(1,nrow(y), 15), labels= s$Date[seq(1, nrow(y), by=15)],
+#      las=2, cex.axis=0.8)
+# legend(1, 75, legend=c("Peso.cosechado.seco..kg.","rain","RH",
+#                        "SRAD","WS2M"),
+#        col=c("black","green", "red", "blue", "purple","brown"), lty=1:2, cex=0.8,
+#        lwd=3)
+# dev.off()
+
+}
+
+
+
+
+
+####____________________
 climate_caldas <- read.csv('climate_caldas.csv', header=TRUE)
 climate_caldas <- climate_caldas[, 2:ncol(climate_caldas)]
 
@@ -91,7 +93,7 @@ s$Date <- factor(s$Date, levels = s$Date)
 
 
 yf <- merge(climate_caldas, cacao_data, by.x='Date', by.y = 'Fecha.de.Floracion',
-           all.x = TRUE)
+            all.x = TRUE)
 #l <- which(yf$flowering != 100)
 #yf$flowering[-l] = 1
 sf <- aggregate(yf, by = list(yf$month, yf$year), FUN=mean, na.rm=TRUE)
