6528fb381d51e95694ff7b5583c63b386ae32482
angeromerov <66690226+angeromerov@users.noreply.github.com>
2021-08-11 14:15:23 +0100

    Update SimpleBcocoa.R

--- SIMPLEcocoa_Narmer/SimpleBcocoa.R
+++ SIMPLEcocoa_Narmer/SimpleBcocoa.R
@@ -60,7 +60,8 @@ GridsimulationSwitch=c('OFF','ON')[1]
 
 ###########load packages######################################################################
 list.of.packages <- c("ggplot2", "plyr","parallel")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,
+                "Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 library(ggplot2)
